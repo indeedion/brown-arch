604984faf0680a0b48e1e26284fc59eafec30cc1
indeedion <mengus00@gmail.com>
2019-05-01 15:30:04 +0200

    finding correct order of steps

--- brown-arch-install.sh
+++ brown-arch-install.sh
@@ -6,11 +6,24 @@
 #global vars
 TMP_PATH="$PWD"
 
+
 #adding user
 read -p "Username: " user
 groupadd $user
 useradd -g $user $user
 passwd $user
+
+#create home directory tree
+mkdir -p /home/$user
+mkdir -p /home/$user/Desktop
+mkdir -p /home/$user/Documents
+mkdir -p /home/$user/Pictures
+mkdir -p /home/$user/Downloads
+
+#setup sudo
+sed -i "/root ALL=(ALL) ALL/a$user ALL=(ALL) ALL" /etc/sudoers
+groupadd sudo
+usermod -a -G sudo $user
 
 #get internet connection
 dhcpcd
@@ -22,38 +35,32 @@ if [ ! ping -c2 8.8.8.8 ]; then
 fi
 
 #upgrade system
+echo "UPGRADING SYSTEM"
 pacman -Syyu
 
+#log into user
+echo "LOG IN AS USER"
+su -$user
+
 #install basic packages
+echo "INSTALLING BASIC PACKAGES"
 packages=$(<arch-packages)
 pacman -S $packages
 
-#create home directory tree
-mkdir -p /home/$user
-mkdir -p /home/$user/Desktop
-mkdir -p /home/$user/Documents
-mkdir -p /home/$user/Pictures
-mkdir -p /home/$user/Downloads
-
 #install vbox guest additions
-packages="virtualbox-guest-iso virtualbox-guest-modules-arch"
-pacman -S $packages
-if pacman -Qi "$packages"; then	
-    mkdir -p /mnt/vboxtmp
-    mount /usr/lib/virtualbox/additions/VBoxGuestAdditions.iso /mnt/vboxtmp
-    cp /mnt/vboxtmp/VBoxLinuxAdditions.run /tmp
-    cd /tmp
-    ./VBoxLinuxAdditions.run &&
-    umount /mnt/vboxtmp
-    rm VBoxLinuxAdditions.run
-else
-    echo "virtualbox guest packages failed to install"
-fi
-
-#setup sudo
-sed -i "/root ALL=(ALL) ALL/a$user ALL=(ALL) ALL" /etc/sudoers
-groupadd sudo
-usermod -a -G sudo $user
+#packages="virtualbox-guest-iso virtualbox-guest-modules-arch"
+#pacman -S $packages
+#if pacman -Qi "$packages"; then	
+#    mkdir -p /mnt/vboxtmp
+#    mount /usr/lib/virtualbox/additions/VBoxGuestAdditions.iso /mnt/vboxtmp
+#    cp /mnt/vboxtmp/VBoxLinuxAdditions.run /tmp
+#    cd /tmp
+#    ./VBoxLinuxAdditions.run &&
+#    umount /mnt/vboxtmp
+#    rm VBoxLinuxAdditions.run
+#else
+#    echo "virtualbox guest packages failed to install"
+#fi
 
 #add blackarch repository
 cd /tmp
@@ -75,6 +82,7 @@ chmod +x strap.sh
 ./strap.sh
 
 #install basic pentesting tools
+echo "INSATLLING PeNtEsTiNg TOOLS"
 packages=$(<barch-packages)
 if [ ! pacman -S $packages ]; then
     echo "failed to install some or alla haxor packages"
@@ -85,7 +93,7 @@ echo "Welcome to the brownest arch!" > /etc/motd
 
 #install fonts
 mkdir -p /usr/share/fonts/TTF
-cp fonts/TTF/* /usr/share/fonts/TTF
+cp ./fonts/TTF/* /usr/share/fonts/TTF
 
 #TO BE CONTINUED
 #do hardening stuff here
